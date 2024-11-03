eb7e3b60ca5de4891b3292a5bad8be5b0748872e
jishaps3@gmail.com <jishaps3@gmail.com>
2024-11-03 14:27:29 +0530

    update footer

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,3 +1,97 @@
+// import React from "react";
+// import { FaFacebookSquare } from "react-icons/fa";
+// import { PiInstagramLogoFill } from "react-icons/pi";
+// import { FaLinkedin } from "react-icons/fa";
+// import { FaYoutube } from "react-icons/fa";
+// import { HiMail } from "react-icons/hi";
+// import { IoLogoWhatsapp } from "react-icons/io";
+
+// import { FaPhoneAlt } from "react-icons/fa";
+// import { FaLocationDot } from "react-icons/fa6";
+// function Footer() {
+//   return (
+//    <div>
+    
+//     <div className="w-full h-full md:h-[450px] sm:h-[400px] lg:h-[500px] xl:h-[550px] bg-[#1F2437] flex flex-col sm:flex-row py-6 sm:py-0 gap-y-10 sm:gap-y-0 ">
+//       <div className="w-full sm:w-[25%] flex justify-center items-start sm:items-center flex-col order-last sm:order-none px-10 sm:px-0">
+//         <img src="/footer_logo.svg" className="size-24 sm:size-36" />
+//         <div className="w-full flex justify-start sm:justify-center items-center text-white mt-8 sm:mt-28 gap-4">
+//           <FaFacebookSquare className="size-6 cursor-pointer hover:scale-110" />
+//           <PiInstagramLogoFill className="size-6 cursor-pointer hover:scale-110" />
+//           <FaLinkedin className="size-6 cursor-pointer hover:scale-110" />
+//           <FaYoutube className="size-6 cursor-pointer hover:scale-110" />
+//         </div>
+//       </div>
+//      <div className=" w-full sm:w-[50%] md:w-[45%] xl:w-[50%] flex">
+//      <div className="w-[50%] flex justify-center  ps-10 flex-col order-first sm:order-none">
+//         <h1 className="text-white font-bold text-base lg:text-xl">QUICK ACCESS</h1>
+//         <ul className="pt-5 text-sm xl:text-base  ">
+//           <li className="pb-3 text-white hover:underline  cursor-pointer "> Home</li>
+//           <li className="pb-3 text-white hover:underline  cursor-pointer">About us</li>
+//           <li className="pb-3 text-white hover:underline  cursor-pointer">Contact</li>
+//           <li className="pb-3 text-white hover:underline  cursor-pointer">Study Abroad</li>
+//           <li className="pb-3 text-white hover:underline  cursor-pointer">Study India</li>
+//           <li className="pb-3 text-white hover:underline  cursor-pointer">Job Abroad</li>
+//           <li className="pb-3 text-white hover:underline  cursor-pointer">Migration</li>
+//         </ul>
+//       </div>
+//       <div className="w-[50%] flex justify-center ps-10 flex-col">
+//         <h1 className="text-white font-bold text-base lg:text-xl">STUDY</h1>
+//         <ul className=" pt-5 text-sm xl:text-base ">
+//           <li className="pb-3 text-white hover:underline  cursor-pointer"> MBBS</li>
+//           <li className="pb-3 text-white hover:underline  cursor-pointer">BSc Nursing</li>
+//           <li className="pb-3 text-white hover:underline  cursor-pointer">GNM</li>
+//           <li className="pb-3 text-white hover:underline  cursor-pointer">Paramedical</li>
+//           <li className="pb-3 text-white hover:underline  cursor-pointer">Pharmacy</li>
+//           <li className="pb-3 text-white hover:underline  cursor-pointer">MBA</li>
+//           <li className="pb-3 text-white hover:underline  cursor-pointer">MSW</li>
+//         </ul>
+//       </div>
+//      </div>
+//       <div className="w-full sm:w-[25%] md:w-[30%] xl:w-[25%] flex items-start sm:justify-center px-10 sm:px-4 xl:px-10 flex-col gap-y-[26px] lg:gap-y-8">
+//         <h1 className="text-white font-bold text-base lg:text-xl">GET IN TOUCH</h1>
+//         <div className="w-full flex  items-center">
+//           <FaPhoneAlt className="size-5 text-white" />
+//           <p className=" text-white text-xs xl:text-base ps-4">
+//             91-90812 34567 <br /> 91-90809 87654
+//           </p>
+//         </div>
+//         <div className="w-full flex items-center">
+//           <HiMail className="size-6 text-white" />
+//           <p className=" text-white text-xs xl:text-base ps-3 sm:ps-0 lg:ps-4 ">
+//             mail@hochinternational.com
+//           </p>
+//         </div>
+//         <div className="w-full flex items-center">
+//           <IoLogoWhatsapp className="size-6  text-white" />
+//           <p className=" text-white text-xs xl:text-base ps-3">
+//             91-90812 34567
+//           </p>
+//         </div>
+//         <div className="w-full flex  items-center">
+//           <FaLocationDot className="size-6 text-white" />
+//           <p className=" text-white text-xs xl:text-base ps-3">
+//             2nd Floor, Anns Tower, MG Road <br /> Angamaly, Ernakulam - 682 018
+//           </p>
+//         </div>
+//       </div>
+//     </div>
+//     <div className="w-full h-10 md:h-20 bg-white flex justify-between items-center px-4 md:px-20">
+//         <p className="text-black text-[10px] sm:text-xs md:text-base xl:text-xl">Copyright 2024 - Hoch International</p>
+//         <p className="text-black text-[10px] sm:text-xs md:text-base xl:text-xl">Powered By <span className="font-[700] antonio " >MCABEE DIGITAL</span> </p>
+
+        
+
+
+//     </div>
+
+//   </div>
+//   );
+// }
+
+// export default Footer;
+
+
 import React from "react";
 import { FaFacebookSquare } from "react-icons/fa";
 import { PiInstagramLogoFill } from "react-icons/pi";
@@ -13,7 +107,7 @@ function Footer() {
    <div>
     
     <div className="w-full h-full sm:h-[400px] lg:h-[500px] bg-[#1F2437] flex flex-col sm:flex-row py-6 sm:py-0 gap-y-10 sm:gap-y-0 ">
-      <div className="w-full sm:w-[25%] flex justify-center items-start sm:items-center flex-col order-last sm:order-none px-10 sm:px-0">
+      <div className="w-full sm:w-[25%] flex mt-0 sm:mt-16 items-start sm:items-center flex-col order-last sm:order-none px-10 sm:px-0">
         <img src="/footer_logo.svg" className="size-24 sm:size-36" />
         <div className="w-full flex justify-start sm:justify-center items-center text-white mt-8 sm:mt-28 gap-4">
           <FaFacebookSquare className="size-6 cursor-pointer hover:scale-110" />
@@ -22,8 +116,8 @@ function Footer() {
           <FaYoutube className="size-6 cursor-pointer hover:scale-110" />
         </div>
       </div>
-     <div className=" w-full sm:w-[50%] flex">
-     <div className="w-[50%] flex justify-center  ps-10 flex-col order-first sm:order-none">
+     <div className=" w-full sm:w-[45%] md:w-[50%] flex">
+     <div className="w-[50%] flex mt-0 sm:mt-16  ps-10 sm:ps-6 md:ps-10 flex-col order-first sm:order-none">
         <h1 className="text-white font-bold text-base lg:text-xl">QUICK ACCESS</h1>
         <ul className="pt-5 text-sm xl:text-base  ">
           <li className="pb-3 text-white hover:underline  cursor-pointer "> Home</li>
@@ -35,7 +129,7 @@ function Footer() {
           <li className="pb-3 text-white hover:underline  cursor-pointer">Migration</li>
         </ul>
       </div>
-      <div className="w-[50%] flex justify-center ps-10 flex-col">
+      <div className="w-[50%] flex mt-0 sm:mt-16 ps-10 sm:ps-6 md:ps-10 flex-col">
         <h1 className="text-white font-bold text-base lg:text-xl">STUDY</h1>
         <ul className=" pt-5 text-sm xl:text-base ">
           <li className="pb-3 text-white hover:underline  cursor-pointer"> MBBS</li>
@@ -48,31 +142,35 @@ function Footer() {
         </ul>
       </div>
      </div>
-      <div className="w-full sm:w-[25%] flex justify-center px-10 sm:px-4 xl:px-10 flex-col gap-y-[26px] lg:gap-y-8">
+      <div className="w-full sm:w-[30%] md:w-[25%]  ps-10 sm:ps-0  flex mt-0 sm:mt-16 px-0 xl:px-10 flex-col gap-y-[26px] lg:gap-y-8">
+        <div className="w-full  flex   flex-col gap-y-[26px] lg:gap-y-8">
         <h1 className="text-white font-bold text-base lg:text-xl">GET IN TOUCH</h1>
         <div className="w-full flex  items-center">
-          <FaPhoneAlt className="size-4 text-white" />
+          <FaPhoneAlt className="size-5 text-white" />
           <p className=" text-white text-xs xl:text-base ps-4">
             91-90812 34567 <br /> 91-90809 87654
           </p>
         </div>
         <div className="w-full flex items-center">
+          <div>
           <HiMail className="size-6 text-white" />
-          <p className=" text-white text-xs xl:text-base ps-3 sm:ps-0 lg:ps-4 ">
+          </div>
+          <p className="  text-white text-xs xl:text-base ps-3 ">
             mail@hochinternational.com
           </p>
         </div>
         <div className="w-full flex items-center">
           <IoLogoWhatsapp className="size-6  text-white" />
-          <p className=" text-white text-xs xl:text-base ps-4">
+          <p className=" text-white text-xs xl:text-base ps-3">
             91-90812 34567
           </p>
         </div>
         <div className="w-full flex  items-center">
           <FaLocationDot className="size-6 text-white" />
-          <p className=" text-white text-xs xl:text-base ps-4">
+          <p className=" text-white text-xs xl:text-base ps-3">
             2nd Floor, Anns Tower, MG Road <br /> Angamaly, Ernakulam - 682 018
           </p>
+        </div>
         </div>
       </div>
     </div>
--- src/app/components/Section.js
+++ src/app/components/Section.js
@@ -1,22 +1,53 @@
-import React from 'react'
+// import React from 'react'
+
+// function Section() {
+//   return (
+//     <div className="w-full">
+//     <div className="w-full  relative">
+//       <div className='w-full'>
+//       <img src="/section1.svg" className='object-cover' />
+
+//       </div>
+
+//       <div className="w-full h-full bg-[#0000005b] absolute top-0 left-0"></div>
+//       <div className="w-[250px] md:w-[550px] xl:w-[660px] px-0 md:px-20 lg:p-0 h-[100px] md:h-[252px] absolute top-12 sm:top-[150px] md:top-[100px] lg:top-[180px] xl:top-[200px] left-12 sm:left-[180px] md:left-[160px]">
+//         <p className="text-white text-[16px] md:text-[30px] lg:text-[40px] xl:text-[58px] leading-[25px] md:leading-[50px] xl:leading-[84px]">
+//         Your Reliable Partner 
+// for Global Education And 
+// Career Opportunities.
+//         </p>
+//       </div>
+//     </div>
+//   </div>
+//   )
+// }
+
+// export default Section
+
+import React from 'react';
 
 function Section() {
   return (
     <div className="w-full">
-    <div className="w-full  relative">
-      <img src="/section1.svg" />
+      <div className="w-full relative">
+        <div className="w-full">
+          <img
+            src="/section1.svg"
+            className="object-cover h-[300px] sm:h-auto" // Set height to 300px below sm breakpoint
+          />
+        </div>
 
-      <div className="w-full h-full bg-[#0000005b] absolute top-0 left-0"></div>
-      <div className="w-[250px] md:w-[550px] xl:w-[660px] px-0 md:px-20 lg:p-0 h-[100px] md:h-[252px] absolute top-12 sm:top-[150px] md:top-[100px] lg:top-[180px] xl:top-[200px] left-12 sm:left-[180px] md:left-[160px]">
-        <p className="text-white text-[16px] md:text-[30px] lg:text-[40px] xl:text-[58px] leading-[25px] md:leading-[50px] xl:leading-[84px]">
-        Your Reliable Partner 
-for Global Education And 
-Career Opportunities.
-        </p>
+        <div className="w-full h-full bg-[#0000005b] absolute top-0 left-0"></div>
+        <div className="w-[250px] md:w-[550px] xl:w-[660px] px-0 md:px-20 lg:p-0 h-[100px] md:h-[252px] absolute top-20 sm:top-[150px] md:top-[100px] lg:top-[180px] xl:top-[200px] left-20 sm:left-[180px] md:left-[160px]">
+          <p className="text-white text-[16px] md:text-[30px] lg:text-[40px] xl:text-[58px] leading-[25px] md:leading-[50px] xl:leading-[84px]">
+            Your Reliable Partner
+            for Global Education And
+            Career Opportunities.
+          </p>
+        </div>
       </div>
     </div>
-  </div>
-  )
+  );
 }
 
-export default Section
+export default Section;
